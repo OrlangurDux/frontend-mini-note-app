8f7f2d01f63618b8ab7d24af03c9b458c8f51e21
Orlangur <kalugin-alexey@lk.market>
2026-09-12 10:10:32 +0300

    [UPDATE] New icon pack

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,8 +9,9 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&family=JetBrains+Mono:wght@500;600&display=swap" rel="stylesheet" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
           <meta name="theme-color" content="#1976d2" />
           <meta name="mobile-web-app-capable" content="yes" />
